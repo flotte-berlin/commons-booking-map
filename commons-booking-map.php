840afc8a96c1e5ee191026fbd915162c6cd2281e
Thomas Richter <thomas.richter.cb@web.de>
2019-05-10 13:14:21 +0200

    plugin check
    * added check for active commons-booking plugin before loading any functionality

--- commons-booking-map.php
+++ commons-booking-map.php
@@ -15,24 +15,29 @@ define( 'CB_MAP_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CB_MAP_ASSETS_URL', plugins_url( 'assets/', __FILE__ ));
 define( 'CB_MAP_LANG_PATH', dirname( plugin_basename( __FILE__ )) . '/languages/' );
 
-require_once( CB_MAP_PATH . 'functions/translate.php' );
+require_once( CB_MAP_PATH . 'functions/is-plugin-active.php' );
 
-load_plugin_textdomain( 'commons-booking-map', false, CB_MAP_LANG_PATH );
+if(cb_map\is_plugin_active('commons-booking.php')) {
 
-require_once( CB_MAP_PATH . 'classes/class-cb-map-shortcode.php' );
+  require_once( CB_MAP_PATH . 'functions/translate.php' );
 
-add_action( 'wp_ajax_cb_map_locations', 'CB_Map_Shortcode::get_locations' );
-add_action( 'wp_ajax_nopriv_cb_map_locations', 'CB_Map_Shortcode::get_locations' );
+  load_plugin_textdomain( 'commons-booking-map', false, CB_MAP_LANG_PATH );
 
-add_shortcode( 'cb_map', 'CB_Map_Shortcode::handle' );
+  require_once( CB_MAP_PATH . 'classes/class-cb-map-shortcode.php' );
 
-// settings
-require_once( CB_MAP_PATH . 'classes/class-cb-map-settings.php' );
-$cb_map_settings = new CB_Map_Settings();
-$cb_map_settings->prepare_settings();
-add_filter( "plugin_action_links_" . plugin_basename( __FILE__ ), array($cb_map_settings, 'add_settings_link') );
+  add_action( 'wp_ajax_cb_map_locations', 'CB_Map_Shortcode::get_locations' );
+  add_action( 'wp_ajax_nopriv_cb_map_locations', 'CB_Map_Shortcode::get_locations' );
 
-//location admininstration
-require_once( CB_MAP_PATH . 'classes/class-cb-map-admin.php' );
-$cb_map_admin = new CB_Map_Admin();
-add_action( 'plugins_loaded', array($cb_map_admin, 'load_special_days_admin'));
+  add_shortcode( 'cb_map', 'CB_Map_Shortcode::handle' );
+
+  // settings
+  require_once( CB_MAP_PATH . 'classes/class-cb-map-settings.php' );
+  $cb_map_settings = new CB_Map_Settings();
+  $cb_map_settings->prepare_settings();
+  add_filter( "plugin_action_links_" . plugin_basename( __FILE__ ), array($cb_map_settings, 'add_settings_link') );
+
+  //location admininstration
+  require_once( CB_MAP_PATH . 'classes/class-cb-map-admin.php' );
+  $cb_map_admin = new CB_Map_Admin();
+  add_action( 'plugins_loaded', array($cb_map_admin, 'load_special_days_admin'));
+}
