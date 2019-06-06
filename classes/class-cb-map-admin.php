732e46f9b6fa3542d409bf6420876b8e5a4cb42a
Thomas Richter <thomas.richter.cb@web.de>
2019-06-06 12:28:46 +0200

    prepare reintroduction of settings 2
    * renamed template file

--- classes/class-cb-map-admin.php
+++ classes/class-cb-map-admin.php
@@ -377,7 +377,7 @@ class CB_Map_Admin {
 
     $data_export_base_url = get_site_url(null, '', null) . '/wp-admin/admin-ajax.php';
 
-    include_once( CB_MAP_PATH . 'templates/settings-page-template.php');
+    include_once( CB_MAP_PATH . 'templates/admin-page-template.php');
 
   }
 
