79a02d87fa26141f474db01442a5757db4c10132
Thomas Richter <thomas.richter.cb@web.de>
2019-11-15 18:55:02 +0100

    bug fix
    added method that was removed by accident when refactoring filters (commit: 30aa38ecb876e9558dca64a9889edd1d8e04e2e6 )

--- classes/class-cb-map-filter.php
+++ classes/class-cb-map-filter.php
@@ -89,6 +89,21 @@ class CB_Map_Filter {
 
     return $result;
   }
+  
+  protected static function check_item_terms_against_categories($item_terms, $category_groups) {
+    $valid_groups_count = 0;
+
+    foreach ($category_groups as $group) {
+      foreach ($item_terms as $term) {
+        if(in_array($term->term_id, $group)) {
+          $valid_groups_count++;
+          break;
+        }
+      }
+    }
+
+    return $valid_groups_count == count($category_groups);
+  }
 }
 
 ?>
